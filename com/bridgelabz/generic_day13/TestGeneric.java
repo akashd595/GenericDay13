72951ffd4a95fe06a450480e8633f437aa521b09
Akash Das <akashd595@gmail.com>
2022-07-21 19:08:38 +0530

    ADDED: Find Greatest among three number

---
+++ com/bridgelabz/generic_day13/TestGeneric.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.generic_day13;
+
+public class TestGeneric {
+    public static int maxNumber(Integer num1, Integer num2, Integer num3){
+        if(num1.compareTo(num2)>0){
+            if(num1.compareTo(num3)>0){
+                return num1;
+            }else{
+                return num3;
+            }
+        }else{
+            if(num2.compareTo(num3)>0){
+                return num2;
+            }else {
+                return num3;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Greatest of 3 number ");
+        Integer num1=50, num2=100, num3=20;
+        Integer max = maxNumber(num1, num2, num3);
+        System.out.println("Max number between "+num1+", "+num2+" and "+num3+" is "+max);
+
+    }
+}
